c8035cf8a8d41c75bdabdf44ab2f314a17020005
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-08-21 08:29:57 +0700

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -46,7 +46,7 @@ function Simple(base, name, trackername, studs)
 	local txtlbl = Instance.new("TextLabel", bb)
 	txtlbl.ZIndex = 10
 	txtlbl.BackgroundTransparency = 1
-	txtlbl.Position = UDim2.new(0, 0, 0, -48)
+	txtlbl.Position = UDim2.new(0, 0, 0, -43)
 	txtlbl.Size = UDim2.new(1, 0, 10, 0)
 	txtlbl.Font = "ArialBold"
 	txtlbl.FontSize = "Size14"
