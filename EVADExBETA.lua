4e861e3bd90c465f469b1f17e5a68932a446b100
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-08-22 15:03:24 +0700

    Update EVADExBETA.lua

--- EVADExBETA.lua
+++ EVADExBETA.lua
@@ -1,1 +1,13 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
+game:GetService("GuiService"):ClearError()
+
+local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Giangplay/Script/main/Orion_Library_PE_V2.lua")))()
+local Window = OrionLib:MakeWindow({IntroText = "Evade v1.0", IntroIcon = "rbxassetid://7733955740",Name = "YOUHUB | Client : ".. identifyexecutor()), HidePremium = false, SaveConfig = false, IntroEnabled = true, ConfigFolder = "evade (Vietnam)"})
+local Tab1 = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
