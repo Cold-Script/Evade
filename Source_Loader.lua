531ba5a22edd27167a66ac62a7f019635c6d9ede
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-09-24 18:51:17 +0700

    Create Source_Loader.lua

---
+++ Source_Loader.lua
@@ -0,0 +1,167 @@
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
+local v = 2
+local Window = redzlib:MakeWindow({"YOUHUB : Evade v".. v, "by white7777", "testando-redzLibv5.json"})
+Window:AddMinimizeButton({
+  Button = { Image = "rbxassetid://5522032314", BackgroundTransparency = 0.25 },
+  Corner = { CornerRadius = UDim.new(0, 5) }
+})
+local Tabs = {
+ Main = Window:MakeTab({"Main", "Home"}),
+ Cheat = Window:MakeTab({"Visual", "Sword"}),
+ Settings = Window:MakeTab({"Setting", "User"})
+}
+Tabs.Main:AddSection({"Farm"})
+_G.C1 = -5000
+Tabs.Main:AddDropdown({
+        Name = "Position X",
+	Description = "Position X",
+        Options = {"-5000", "-2500", "0", "2500", "5000"},
+        MultiSelect = false,
+        Default = {"-5000"},
+	Callback = function(value)
+_G.C1 = value			
+end})
+_G.C2 = 7500
+Tabs.Main:AddDropdown({
+        Name = "Position Y",
+	Description = "Position Y",
+        Options = {"7500", "15000", "30000"},
+        MultiSelect = false,
+        Default = {"7500"},
+	Callback = function(value)
+_G.C2 = value			
+end})
+_G.C3 = -5000
+Tabs.Main:AddDropdown({
+        Name = "Position Z",
+	Description = "Position Z",
+        Options = {"-5000", "-2500", "0", "2500", "5000"},
+        MultiSelect = false,
+        Default = {"-5000"},
+	Callback = function(value)
+_G.C3 = value			
+end})
+Tabs.Main:AddToggle({
+	Name = "AFK Farm",
+	Description = "Teleport Far to AFK",
+	Callback=function(value)
+_G.AFK = value
+while _G.AFK do wait(3)
+spawn(function()
+pcall(function()
+game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart').CFrame = CFrame.new(_G.C1, _G.C2, _G.C3)
+wait(0.25)
+game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart').CFrame = CFrame.new(-50000, _G.C2, -50000)
+end)
+end)
+end
+end})
+Tabs.Main:AddButton({
+        Name = "Teleport Map",
+        Description = "Teleport map in play",
+        Callback = function()
+game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart').CFrame = CFrame.new(0, 0, 0)
+end})
+Tabs.Main:AddSection({"Players"})
+_G.ToggleSpeed = 0
+Tabs.Main:AddDropdown({
+  Name = "Speed Boost",
+  Description = "Select a Boost",
+  Options = {"0","25","50","75","100","125","150","175","200"},
+  Default = {"0"},
+  MultiSelect = false,
+  Callback = function(value)
+_G.ToggleSpeed = value
+game:GetService("RunService").RenderStepped:Connect(function()
+			pcall(function()
+				if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
+					game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * _G.ToggleSpeed/100)
+				end
+			end)
+    end)
+end})
+Tabs.Main:AddToggle({
+        Name = "Respawn On Downed",
+	Description = "When Downed Auto Respawn",	
+	Callback = function(value)
+_G.Rep = value
+while _G.Rep do wait()
+if game.Players.LocalPlayer.Character:GetAttribute("Downed") then
+game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
+end
+end 
+end})
+Tabs.Main:AddButton({
+        Name = "Respawn",
+        Description = "In Main Menu Click To Playing",
+        Callback = function()
+game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
+end})
+Tabs.Main:AddSection({"Auto"})
+Tabs.Main:AddToggle({
+	Name = "Auto Whistle",
+	Description = "Auto Whistle",
+	Callback=function(value)
+_G.Whi = value
+while _G.Whi do wait()
+spawn(function()
+pcall(function()
+game:GetService("Players").LocalPlayer.PlayerScripts.Events.KeybindUsed:Fire("Whistle", true)
+game:GetService("ReplicatedStorage").Events.Whistle:FireServer()			
+end)
+end)
+end        
+end})
+Tabs.Main:AddToggle({
+	Name = "Auto Drink Cola",
+	Description = "Drink Cola (Can Complete Quest)",
+	Callback=function(value)
+_G.Drink = value
+while _G.Drink do wait()
+spawn(function()
+pcall(function()
+game:GetService("ReplicatedStorage").Events.UseUsable:FireServer("Cola")
+end)
+end)
+end        
+end})
+Tabs.Main:AddSection({"Camera"})
+_G.FOV = 70
+Tabs.Main:AddDropdown({
+  Name = "FieldOfView",
+  Description = "Select a Field Of View",
+  Options = {"70","80","90","100","110","120"},
+  Default = {"70"},
+  MultiSelect = false,
+  Callback = function(value)
+_G.FOV = value
+game:GetService("RunService").RenderStepped:Connect(function()
+			pcall(function()
+				workspace.CurrentCamera.FieldOfView = _G.FOV
+			end)
+    end)
+end})
+Tabs.Main:AddToggle({
+	Name = "Auto No Shake Camera",
+	Description = "Auto No Shake Camera",
+	Callback=function(value)
+_G.NoC = value			
+while _G.NoC do wait()
+spawn(function()
+pcall(function()
+game.Players.LocalPlayer.PlayerScripts.CameraShake.Value = CFrame.new(0,0,0) * CFrame.new(0,0,0)
+end)
+end)
+end
+end})
+Tabs.Main:AddToggle({
+	Name = "Fast Revive",
+	Description = "Revive all Players Fast",
+	Callback = function(value)
+if value then
+workspace.Game.Settings:SetAttribute("ReviveTime", 2.2)
+			else
+workspace.Game.Settings:SetAttribute("ReviveTime", 3)				
+end
+end})
+
