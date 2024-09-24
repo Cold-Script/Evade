fcc9cf2355011f8296d6f33e8a23ad6c6d31b9a0
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-09-24 19:28:53 +0700

    Update Source_Loader.lua

--- Source_Loader.lua
+++ Source_Loader.lua
@@ -1,3 +1,82 @@
+local function Billboard(child, name, color, title)
+local Billboard = Instance.new("BillboardGui");do
+Billboard.Active = true;
+Billboard.AlwaysOnTop = true;
+Billboard.ClipsDescendants = true;
+Billboard.LightInfluence = 1;
+Billboard.Size = UDim2.new(10000, 0, 10000, 0);
+Billboard.ResetOnSpawn = false;
+Billboard.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
+Billboard.Parent = child;
+Billboard.Name = title;
+local Title = Instance.new("TextLabel");
+Title.TextSize = 15;
+Title.Font = "RobotoCondensed";
+Title.TextColor3 = color;
+Title.BackgroundColor3 = Color3.new(1, 1, 1);
+Title.BackgroundTransparency = 1;
+Title.BorderColor3 = Color3.new(0, 0, 0);
+Title.BorderSizePixel = 0;
+Title.Size = UDim2.new(1, 0, 1, 0);
+Title.Visible = true;
+Title.Parent = Billboard;
+local uistroke = Instance.new("UIStroke");
+uistroke.Thickness = 1;
+uistroke.Parent = Title;
+task.spawn(function()
+game:GetService("RunService").RenderStepped:Connect(function()
+Title.Text = string.format("%s\n[%sm]", name or child.Name, math.floor((workspace.CurrentCamera.CFrame.Position - child:GetPivot().Position).Magnitude));
+end);
+end);
+end
+end
+local function CylinderESP(child, name, color, title)
+Billboard(child, name, color, title)
+local Adornment = Instance.new("CylinderHandleAdornment")
+Adornment.Height = child.Size.Y * 2
+Adornment.Radius = child.Size.X * 2
+Adornment.CFrame = CFrame.new(0,0,0) * CFrame.Angles(math.rad(90), 0, 0)
+Adornment.Color3 = color
+Adornment.Transparency = 0.65
+Adornment.AlwaysOnTop = true
+Adornment.ZIndex = 10
+Adornment.Adornee = child
+Adornment.Name = title
+Adornment.Parent = child
+end
+local function Billboard2(child, name, color, title)
+local Billboard = Instance.new("BillboardGui");do
+Billboard.Active = true;
+Billboard.AlwaysOnTop = true;
+Billboard.ClipsDescendants = true;
+Billboard.LightInfluence = 1;
+Billboard.Size = UDim2.new(10000, 0, 10000, 35);
+Billboard.ResetOnSpawn = false;
+Billboard.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
+Billboard.Parent = child;
+Billboard.Name = title;
+local Title = Instance.new("TextLabel");
+Title.TextSize = 15;
+Title.Font = "RobotoCondensed";
+Title.TextColor3 = color;
+Title.BackgroundColor3 = Color3.new(1, 1, 1);
+Title.BackgroundTransparency = 1;
+Title.BorderColor3 = Color3.new(0, 0, 0);
+Title.BorderSizePixel = 0;
+Title.Size = UDim2.new(1, 0, 1, 0);
+Title.Visible = true;
+Title.Parent = Billboard;
+local uistroke = Instance.new("UIStroke");
+uistroke.Thickness = 1;
+uistroke.Parent = Title;
+task.spawn(function()
+game:GetService("RunService").RenderStepped:Connect(function()
+Title.Text = string.format("%s\n[%sm][Downed]", name or child.Name, math.floor((workspace.CurrentCamera.CFrame.Position - child:GetPivot().Position).Magnitude));
+end);
+end);
+end
+end
+
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
 local v = 2
 local Window = redzlib:MakeWindow({"YOUHUB : Evade v".. v, "by white7777", "testando-redzLibv5.json"})
@@ -73,12 +152,12 @@ Tabs.Main:AddDropdown({
   Callback = function(value)
 _G.ToggleSpeed = value
 game:GetService("RunService").RenderStepped:Connect(function()
-			pcall(function()
-				if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
-					game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * _G.ToggleSpeed/100)
-				end
-			end)
-    end)
+pcall(function()
+if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
+game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * _G.ToggleSpeed/100)
+end
+end)
+end)
 end})
 Tabs.Main:AddToggle({
         Name = "Respawn On Downed",
@@ -136,14 +215,14 @@ Tabs.Main:AddDropdown({
   Callback = function(value)
 _G.FOV = value
 game:GetService("RunService").RenderStepped:Connect(function()
-			pcall(function()
-				workspace.CurrentCamera.FieldOfView = _G.FOV
-			end)
-    end)
+pcall(function()
+workspace.CurrentCamera.FieldOfView = _G.FOV
+end)
+end)
 end})
 Tabs.Main:AddToggle({
-	Name = "Auto No Shake Camera",
-	Description = "Auto No Shake Camera",
+	Name = "No Shake Camera",
+	Description = "No Shake Camera",
 	Callback=function(value)
 _G.NoC = value			
 while _G.NoC do wait()
@@ -154,6 +233,7 @@ end)
 end)
 end
 end})
+Tabs.Main:AddSection({"Benefit"})
 Tabs.Main:AddToggle({
 	Name = "Fast Revive",
 	Description = "Revive all Players Fast",
@@ -164,4 +244,118 @@ workspace.Game.Settings:SetAttribute("ReviveTime", 2.2)
 workspace.Game.Settings:SetAttribute("ReviveTime", 3)				
 end
 end})
-
+Tabs.Cheat:AddToggle({
+	Name = "Disabled Invis Barrier",
+	Description = "Disabled Invisible Parts",
+	Callback = function(value)
+_G.ReIn = value
+while _G.ReIn do wait(1)
+spawn(function()
+pcall(function()
+for _,v in pairs(workspace.Game.Map:GetDescendants()) do
+if v.Name == "InvisParts" then
+v:Destroy()
+end
+end
+end)	
+end)						
+end				
+end})
+Tabs.Cheat:AddToggle({
+	Name = "Disabled Void ROBLOX",
+	Description = "Disabled Void ROBLOX",
+	Callback = function(value)
+_G.ReIn = value
+while _G.ReIn do wait(1)
+spawn(function()
+pcall(function()
+workspace.FallenPartsDestroyHeight = 0 / 0
+end)	
+end)						
+end				
+end})
+Tabs.Cheat:AddToggle({
+	Name = "Disabled Glass Wall",
+	Description = "Disabled Glass",
+	Callback = function(value)
+_G.ReIn = value
+while _G.ReIn do wait()
+spawn(function()
+pcall(function()
+for _,v in pairs(workspace.Game.Map:GetDescendants()) do
+if v.Name == "Glass" then
+v:Destroy()
+end
+end
+end)	
+end)						
+end				
+end})
+Tabs.Main:AddSection({"Lighting"})
+Tabs.Main:AddToggle({
+	Name = "Fullbright",
+	Description = "Brightness Screen Players",
+	Callback=function(value)
+if value then
+game.Lighting.Brightness = 2
+game.Lighting.GlobalShadows = false
+game.Lighting.OutdoorAmbient = Color3.new(1,1,1)
+else				
+game.Lighting.Brightness = 2
+game.Lighting.GlobalShadows = true
+game.Lighting.OutdoorAmbient = Color3.new(0.5,0.5,0.5)
+end
+end})
+Tabs.Main:AddToggle({
+	Name = "Fog Remover",
+	Description = "Fog Remover",
+	Callback=function(value)
+if value then
+game.Lighting.FogStart = 0
+game.Lighting.FogEnd = 9e9				
+else
+game.Lighting.FogStart = 100			
+game.Lighting.FogEnd = 10000
+end
+end})
+Tabs.Visual:AddSection({"ESP"})
+Tabs.Visual:AddToggle({
+	Name = "ESP Bot & Players",
+	Description = "ESP Bots & Players",
+	Callback = function(value)
+if value then
+pcall(function()
+for _,v in pairs(workspace.Game.Players:GetDescendants()) do
+if v.Name == "HumanoidRootPart" then
+CylinderESP(v, v.Parent.Name, v.Color, "ESP01")
+end
+end
+end)
+else
+for _,v in pairs(workspace:GetDescendants()) do
+if v.Name == "ESP01" then
+v:Destroy()
+end
+end
+end
+end})
+Tabs.Visual:AddToggle({
+	Name = "ESP Players On Downed",
+	Description = "ESP Players Down",
+	Callback = function(value)
+if value then
+pcall(function()
+for _,v in pairs(workspace.Game.Players:GetDescendants()) do
+if v:GetAttribute('Downed') then
+Billboard2(v, "", Color3.fromRGB(125, 249, 255), "ESP02")
+end
+end
+end)
+else
+for _,v in pairs(workspace:GetDescendants()) do
+if v.Name == "ESP02" then
+v:Destroy()
+end
+end
+end
+end})
