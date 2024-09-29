26a912280b8f8dfe89439d61790e321b2642ecd3
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-09-29 08:01:45 +0700

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -1,14 +1,15 @@
 --EZ EVADE
+game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
 game:GetService("RunService").RenderStepped:Connect(function()			
 pcall(function()	
 if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
-game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * 10/100)
+game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * 5/100)
 end
 if game.Players.LocalPlayer.Character:GetAttribute("Downed") then
 game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
 end
 game.Players.LocalPlayer.PlayerScripts.CameraShake.Value = CFrame.new(0,0,0) * CFrame.new(0,0,0)
-workspace.Game.Settings:SetAttribute("ReviveTime", 0)
+workspace.Game.Settings:SetAttribute("ReviveTime", 1.95)
 end)
 end)
 local IJ = true
@@ -18,4 +19,4 @@ game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid
 end
 end)
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/ui-libraries/main/dxhooknotify/src.lua", true))()
-l:Notify("YPoint / Evade","Evade EZ",10)
+library:Notify("YPoint / Evade","Evade is EZ",10)
