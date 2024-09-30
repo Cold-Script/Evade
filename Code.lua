72d48bd8f2a82ebc69a7029813750dbc2503f4c3
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-09-30 16:28:58 +0700

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -1,17 +1,60 @@
 --EZ EVADE
-function Distance(part, extra)
-	if not extra then extra = 15 end
-	if not game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") or not part then
-		return false
-	end
-	local distanceToPart = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - part.Position).magnitude
-	if distanceToPart <= extra then
-		return true
-	end
-	return false
+local function Billboard(child, name, title)
+bill = Instance.new("BillboardGui", game.CoreGui)
+        bill.AlwaysOnTop = true
+        bill.Size = UDim2.new(0,400,0,100)
+        bill.Adornee = child
+        bill.MaxDistance = 2000
+        bill.Name = title
+
+        local mid = Instance.new("Frame",bill)
+        mid.AnchorPoint = Vector2.new(0.5,0.5)
+        mid.BackgroundColor3 = Color3.new(1,1,1)
+        mid.Size = UDim2.new(0,12.5,0,12.5)
+        mid.Position = UDim2.new(0.5,0,0.5,0)
+        Instance.new("UICorner",mid).CornerRadius = UDim.new(100,0)
+        Instance.new("UIStroke",mid)
+
+        local txt = Instance.new("TextLabel",bill)
+        txt.AnchorPoint = Vector2.new(0.5,0.5)
+        txt.BackgroundTransparency = 1
+        txt.BackgroundColor3 = Color3.new(0,0,0)
+        txt.TextColor3 = Color3.new(1,1,1)
+        txt.Font = "GothamBold"
+        txt.TextSize = "15"
+        txt.Size = UDim2.new(1,0,0,-50)
+        txt.Position = UDim2.new(0.5,0,0.7,0)
+        Instance.new("UIStroke",txt)
+task.spawn(function()
+game:GetService("RunService").RenderStepped:Connect(function()
+txt.Text = string.format("%s\n[%s studs]", name or child.Name, math.floor((game.Players.LocalPlayer.Character.Head.Position - child:GetPivot().Position).Magnitude));
+end);
+end);
 end
+function Distance(part, extra)
+if not extra then extra = 15 end
+if not game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") or not part then
+return false
+end
+local distanceToPart = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - part.Position).magnitude
+if distanceToPart <= extra then
+return true
+end
+return false
+end
+task.spawn(function()
+while task.wait() do
 pcall(function()
-for _,v in pairs(workspace.Game.Players:GetDescendants()) do
+for i, v in pairs(workspace.Game.Players:GetChildren()) do
+if not game.Players:FindFirstChild(v.Name) then
+Billboard(v.HumanoidRootPart, v.Name, "Bot_ESP")
+end							
+end
+end)
+end
+end)
+pcall(function()
+for _,v in pairs(workspace.Game.Players:GetChildren()) do
 if not game.Players:FindFirstChild(v.Name) and Distance(v:FindFirstChildWhichIsA("BasePart"), 55) then
 game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart').CFrame = CFrame.new(0, 100000, 0)
 end
